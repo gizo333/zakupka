e6b952b214d55cba118d47aaad41c766a5b36252
Artem <gizo@MacBook-Air-Artem.local>
2023-06-28 15:49:46 +0300

    version 0.29 search to list_to_rest

--- lib/list_to_rest/list_to_rest.dart
+++ lib/list_to_rest/list_to_rest.dart
@@ -11,9 +11,10 @@ class RestaurantListPage extends StatefulWidget {
 
 class _RestaurantListPageState extends State<RestaurantListPage> {
   bool buttonState = true;
+  String searchQuery = '';
+
   FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
-  Map<String, bool> joinRequests = {
-  }; // Словарь для хранения состояний запросов для каждого ресторана
+  Map<String, bool> joinRequests = {};
 
   Future<List<String>> fetchRestaurants() async {
     final postgresConnection = PostgreSQLConnection(
@@ -27,7 +28,8 @@ class _RestaurantListPageState extends State<RestaurantListPage> {
     try {
       await postgresConnection.open();
       final results = await postgresConnection.query(
-          'SELECT restaurant FROM restaurant');
+          'SELECT restaurant FROM restaurant WHERE restaurant ILIKE @query',
+          substitutionValues: {'query': '%$searchQuery%'}); // Используем ILIKE для регистро-независимого поиска
       postgresConnection.close();
 
       final list = results.map((row) => row[0] as String).toList();
@@ -68,7 +70,6 @@ class _RestaurantListPageState extends State<RestaurantListPage> {
       await postgresConnection.close();
     }
   }
-
 
   Future<void> sendJoinRequest(String restaurantName, String userFullName,
       String userId) async {
@@ -146,7 +147,6 @@ class _RestaurantListPageState extends State<RestaurantListPage> {
     }
   }
 
-
   @override
   Widget build(BuildContext context) {
     final _restaurantListProvider = Provider.of<RestaurantListProvider>(
@@ -162,78 +162,111 @@ class _RestaurantListPageState extends State<RestaurantListPage> {
           },
         ),
       ),
-      body: FutureBuilder<List<String>>(
-        future: fetchRestaurants(),
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return Center(
-              child: CircularProgressIndicator(),
-            );
-          } else if (snapshot.hasData) {
-            final restaurants = snapshot.data!;
-            return FutureBuilder<User?>(
-              future: _firebaseAuth
-                  .authStateChanges()
-                  .first,
-              builder: (context, userSnapshot) {
-                if (userSnapshot.connectionState == ConnectionState.waiting) {
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: TextField(
+              onChanged: (value) {
+                setState(() {
+                  searchQuery = value;
+                });
+              },
+              decoration: InputDecoration(
+                labelText: 'Поиск ресторанов',
+                suffixIcon: Icon(Icons.search),
+              ),
+            ),
+          ),
+          Expanded(
+            child: FutureBuilder<List<String>>(
+              future: fetchRestaurants(),
+              builder: (context, snapshot) {
+                if (snapshot.connectionState == ConnectionState.waiting) {
                   return Center(
                     child: CircularProgressIndicator(),
                   );
-                } else if (userSnapshot.hasData) {
-                  final userId = userSnapshot.data!.uid;
-                  return FutureBuilder<String>(
-                    future: getUserFullName(userId),
-                    builder: (context, fullNameSnapshot) {
-                      if (fullNameSnapshot.connectionState ==
+                } else if (snapshot.hasData) {
+                  final restaurants = snapshot.data!;
+                  return FutureBuilder<User?>(
+                    future: _firebaseAuth
+                        .authStateChanges()
+                        .first,
+                    builder: (context, userSnapshot) {
+                      if (userSnapshot.connectionState ==
                           ConnectionState.waiting) {
                         return Center(
                           child: CircularProgressIndicator(),
                         );
-                      } else if (fullNameSnapshot.hasData) {
-                        final userFullName = fullNameSnapshot.data!;
-                        return ListView.builder(
-                          itemCount: restaurants.length,
-                          itemBuilder: (context, index) {
-                            final name = restaurants[index];
-                            final isRequestSent =
-                                _restaurantListProvider.getRequestStatus(
-                                    name, userId) == 'pending';
+                      } else if (userSnapshot.hasData) {
+                        final userId = userSnapshot.data!.uid;
+                        return FutureBuilder<String>(
+                          future: getUserFullName(userId),
+                          builder: (context, fullNameSnapshot) {
+                            if (fullNameSnapshot.connectionState ==
+                                ConnectionState.waiting) {
+                              return Center(
+                                child: CircularProgressIndicator(),
+                              );
+                            } else if (fullNameSnapshot.hasData) {
+                              final userFullName = fullNameSnapshot.data!;
+                              return ListView.builder(
+                                itemCount: restaurants.length,
+                                itemBuilder: (context, index) {
+                                  final name = restaurants[index];
+                                  final isRequestSent =
+                                      _restaurantListProvider.getRequestStatus(
+                                          name, userId) == 'pending';
 
-                            return Column(
-                              children: [
-                                ListTile(
-                                  title: Text(name),
-                                  trailing: isRequestSent
-                                      ? ElevatedButton(
-                                    onPressed: () {
-                                      cancelJoinRequest(name, userId);
-                                    },
-                                    child: Text('Отменить'),
-                                  )
-                                      : ElevatedButton(
-                                    onPressed: () {
-                                      setState(() {
-                                        buttonState = true;
-                                      });
-                                      sendJoinRequest(
-                                          name, userFullName, userId);
-                                    },
-                                    child: Text('Вступить'),
-                                  ),
-                                ),
-                                Divider(
-                                  height: 1,
-                                  thickness: 1,
-                                  color: Colors.grey,
-                                ),
-                              ],
+                                  return Column(
+                                    children: [
+                                      ListTile(
+                                        title: Text(name),
+                                        trailing: isRequestSent
+                                            ? ElevatedButton(
+                                          onPressed: () {
+                                            cancelJoinRequest(
+                                                name, userId);
+                                          },
+                                          child: Text('Отменить'),
+                                        )
+                                            : ElevatedButton(
+                                          onPressed: () {
+                                            setState(() {
+                                              buttonState = true;
+                                            });
+                                            sendJoinRequest(
+                                                name,
+                                                userFullName,
+                                                userId);
+                                          },
+                                          child: Text('Вступить'),
+                                        ),
+                                      ),
+                                      Divider(
+                                        height: 1,
+                                        thickness: 1,
+                                        color: Colors.grey,
+                                      ),
+                                    ],
+                                  );
+                                },
+                              );
+                            } else if (fullNameSnapshot.hasError) {
+                              return Center(
+                                child: Text(
+                                    'Error: ${fullNameSnapshot.error}'),
+                              );
+                            }
+
+                            return Center(
+                              child: Text('No data available'),
                             );
                           },
                         );
-                      } else if (fullNameSnapshot.hasError) {
+                      } else if (userSnapshot.hasError) {
                         return Center(
-                          child: Text('Error: ${fullNameSnapshot.error}'),
+                          child: Text('Error: ${userSnapshot.error}'),
                         );
                       }
 
@@ -242,9 +275,9 @@ class _RestaurantListPageState extends State<RestaurantListPage> {
                       );
                     },
                   );
-                } else if (userSnapshot.hasError) {
+                } else if (snapshot.hasError) {
                   return Center(
-                    child: Text('Error: ${userSnapshot.error}'),
+                    child: Text('Error: ${snapshot.error}'),
                   );
                 }
 
@@ -252,17 +285,9 @@ class _RestaurantListPageState extends State<RestaurantListPage> {
                   child: Text('No data available'),
                 );
               },
-            );
-          } else if (snapshot.hasError) {
-            return Center(
-              child: Text('Error: ${snapshot.error}'),
-            );
-          }
-
-          return Center(
-            child: Text('No data available'),
-          );
-        },
+            ),
+          ),
+        ],
       ),
     );
   }
