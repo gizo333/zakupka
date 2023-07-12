9c8fc1bb8fd2cfb03588db3cb523b670b120b0e5
Artem <gizo@MacBook-Air-Artem.local>
2023-07-12 23:02:50 +0300

    Для браузера реализована функция список ресторанов и поиска

--- lib/connect_BD/connect_web.dart
+++ lib/connect_BD/connect_web.dart
@@ -1,6 +1,31 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
 
+
+
+Future<Map<String, dynamic>> getUserData(String tableName, String userId) async {
+  final url = Uri.parse('http://37.140.241.144:8080/api/$tableName/user_id/$userId');
+
+  try {
+    final response = await http.get(url);
+
+    if (response.statusCode == 200) {
+      final data = jsonDecode(response.body);
+      return data as Map<String, dynamic>;
+    } else if (response.statusCode == 404) {
+      print('Пользователь не найден');
+      return {}; // Возвращаем пустую карту вместо null
+    } else {
+      throw Exception('Ошибка при получении данных: ${response.statusCode}');
+    }
+  } catch (e) {
+    throw Exception('Ошибка при выполнении запроса: $e');
+  }
+}
+
+
+
+
 Future<List<dynamic>> getDataFromServer(String tableName, String fieldName) async {
   final url = Uri.parse('http://37.140.241.144:8080/api/$tableName/$fieldName');
 
