3aaedcb31e6aaa2dfb18b4f546dbbc485c10ab79
Danila Kiva <kivadan@inbox.ru>
2023-07-12 18:31:10 +0300

    zalupka pupka

--- lib/table/save_to_bd.dart
+++ lib/table/save_to_bd.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:postgres/postgres.dart';
 
-Future<void> saveDataToPostgreSQL(BuildContext context, List<dynamic> _lists) async {
+Future<void> saveDataToPostgreSQLB(List<dynamic> _lists, String tablename) async {
   final connection = PostgreSQLConnection(
     '37.140.241.144',
     5432,
@@ -14,18 +14,22 @@ Future<void> saveDataToPostgreSQL(BuildContext context, List<dynamic> _lists) as
     await connection.open();
 
     // Очистить таблицу перед импортом (если нужно)
-    await connection.execute('DELETE FROM position');
+    await connection.execute('DELETE FROM $tablename');
 
     // Создать список значений для вставки
     final values = _lists
-        .map((position) => '(${position.code}, \'${_escapeString(position.name)}\', ${position.ml}, ${position.itog})')
+        .asMap()
+        .entries
+        .map((entry) =>
+            '(${entry.key + 1}, ${entry.value.code}, \'${_escapeString(entry.value.name)}\', ${entry.value.ml}, ${entry.value.itog})')
         .join(', ');
 
     // Замерить время сохранения данных
     final stopwatch = Stopwatch()..start();
 
     // Вставить данные в таблицу
-    await connection.execute('INSERT INTO position (code, name, ml, itog) VALUES $values');
+    await connection.execute(
+        'INSERT INTO $tablename (id, code, name, ml, itog) VALUES $values');
 
     // Остановить и вывести время сохранения в консоль
     stopwatch.stop();
@@ -35,22 +39,12 @@ Future<void> saveDataToPostgreSQL(BuildContext context, List<dynamic> _lists) as
     await connection.close();
 
     // Очистить списки после сохранения данных в БД
-    _lists.clear();
-
-    ScaffoldMessenger.of(context).showSnackBar(
-      SnackBar(
-        content: Text('Данные сохранены в PostgreSQL'),
-      ),
-    );
+    // _lists.clear();
   } catch (e) {
     print('Ошибка при сохранении данных в PostgreSQL: $e');
-    ScaffoldMessenger.of(context).showSnackBar(
-      SnackBar(
-        content: Text('Ошибка при сохранении данных в PostgreSQL'),
-      ),
-    );
   }
 }
+
 // _escapeString используется для экранирования специальных символов в строке.
 // Экранирование символов осуществляется путем замены одинарных кавычек (') на двойные кавычки (''),
 // чтобы предотвратить ошибки при выполнении SQL-запросов.
